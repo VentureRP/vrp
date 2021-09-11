760bf2aa7c029ba6d3ae4649144ab03f74c07df0
Tony <1414927+zfbx@users.noreply.github.com>
2021-09-11 02:24:13 -0400

    moved to private repo

--- resources/[vr]/vr-afk/server/main.lua
+++
@@ -1,9 +0,0 @@
-RegisterServerEvent("KickForAFK")
-AddEventHandler("KickForAFK", function()
-	DropPlayer(source, "You Have Been Kicked For Being AFK")
-end)
-
-VRCore.Functions.CreateCallback('vr-afkkick:server:GetPermissions', function(source, cb)
-    local group = VRCore.Functions.GetPermission(source)
-    cb(group)
-end)
