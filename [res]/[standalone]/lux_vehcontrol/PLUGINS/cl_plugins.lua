73ac554e74d78244e63df9650ecc561b3fa2134c
Tony <1414927+zfbx@users.noreply.github.com>
2021-09-11 02:24:49 -0400

    lux vehicle control

---
+++ [res]/[standalone]/lux_vehcontrol/PLUGINS/cl_plugins.lua
@@ -0,0 +1,52 @@
+--[[
+---------------------------------------------------
+LUXART VEHICLE CONTROL V3 (FOR FIVEM)
+---------------------------------------------------
+Coded by Lt.Caine
+ELS Clicks by Faction
+Additional Modification by TrevorBarns
+---------------------------------------------------
+FILE: cl_plugins.lua
+PURPOSE: Builds RageUI Plugin Menu based on plugins 
+settings. Handles Plugin -> LVC event communication
+---------------------------------------------------
+]]
+-- RAGE UI
+--	Draws specific button with callback to plugins menu if the plugin is found and enabled. (controlled in plugins settings file)
+CreateThread(function()
+    while plugins_installed do
+	    RageUI.IsVisible(RMenu:Get('lvc', 'plugins'), function()
+			-----------------------------------------------------------------------------------------------------------------
+			if smart_siren_masterswitch then
+				RageUI.Button('Smart Siren Settings', "Open smart siren settings menu. (smart_sirens)", {RightLabel = "→→→"}, true, {
+				  onSelected = function()
+				  end,
+				}, RMenu:Get('lvc', 'smartsiren'))	
+			end
+			-----------------------------------------------------------------------------------------------------------------
+			if tkd_masterswitch then
+				RageUI.Button('Takedown Settings', "Open takedown lights menu. (takedowns)", {RightLabel = "→→→"}, true, {
+				  onSelected = function()
+				  end,
+				}, RMenu:Get('lvc', 'tkdsettings'))	
+			end
+			-----------------------------------------------------------------------------------------------------------------
+			if ei_masterswitch then
+				RageUI.Button('Extra Integration Settings', "Open extra integration menu. (extra_integration)", {RightLabel = "→→→"}, true, {
+				  onSelected = function()
+				  end,
+				}, RMenu:Get('lvc', 'extrasettings'))	
+			end		
+			-----------------------------------------------------------------------------------------------------------------
+		end)
+        Wait(0)
+	end
+end)
+
+-- FUNCTIONS
+--	IsPluginMenuOpen is called inside IsMenuOpen (LVC/UI/cl_ragemenu.lua) to separate them, this is useful for plugin updates separate of main LVC updates.
+function IsPluginMenuOpen()
+	return 	RageUI.Visible(RMenu:Get('lvc', 'smartsiren')) or 
+			RageUI.Visible(RMenu:Get('lvc', 'tkdsettings')) or 
+			RageUI.Visible(RMenu:Get('lvc', 'extrasettings')) 		
+end
---
+++ [res]/[standalone]/lux_vehcontrol/UTIL/sv_lvc.lua
@@ -0,0 +1,135 @@
+--[[
+---------------------------------------------------
+LUXART VEHICLE CONTROL V3 (FOR FIVEM)
+---------------------------------------------------
+Coded by Lt.Caine
+ELS Clicks by Faction
+Additional Modification by TrevorBarns
+---------------------------------------------------
+FILE: server.lua
+PURPOSE: Handle version checking, syncing vehicle 
+states.
+---------------------------------------------------
+]]
+
+local curr_version = GetResourceMetadata(GetCurrentResourceName(), 'version', 0)
+local experimental = GetResourceMetadata(GetCurrentResourceName(), 'experimental', 0) == 'true' 
+
+local plugin_count = 0
+local plugins_cv = { }		-- table of active plugins current versions plugins_cv = { ['<pluginname>'] = <version> }
+local plugins_rv = { }		-- table of active plugins repository versions
+
+---------------VEHICLE STATE EVENTS----------------
+RegisterServerEvent("lvc_GetRepoVersion_s") 
+AddEventHandler("lvc_GetRepoVersion_s", function()
+    TriggerClientEvent("lvc_SendRepoVersion_c", source, repo_version)
+end)
+
+RegisterServerEvent("lvc_TogDfltSrnMuted_s")
+AddEventHandler("lvc_TogDfltSrnMuted_s", function(toggle)
+	TriggerClientEvent("lvc_TogDfltSrnMuted_c", -1, source, toggle)
+end)
+
+RegisterServerEvent("lvc_SetLxSirenState_s")
+AddEventHandler("lvc_SetLxSirenState_s", function(newstate)
+	TriggerClientEvent("lvc_SetLxSirenState_c", -1, source, newstate)
+end)
+
+RegisterServerEvent("lvc_SetPwrcallState_s")
+AddEventHandler("lvc_SetPwrcallState_s", function(newstate)
+	TriggerClientEvent("lvc_SetPwrcallState_c", -1, source, newstate)
+end)
+
+RegisterServerEvent("lvc_SetAirManuState_s")
+AddEventHandler("lvc_SetAirManuState_s", function(newstate)
+	TriggerClientEvent("lvc_SetAirManuState_c", -1, source, newstate)
+end)
+
+RegisterServerEvent("lvc_TogIndicState_s")
+AddEventHandler("lvc_TogIndicState_s", function(newstate)
+	TriggerClientEvent("lvc_TogIndicState_c", -1, source, newstate)
+end)
+
+-------------VERSION CHECKING & STARTUP------------
+--	PLUGIN VERSION CHECKING EVENT	
+RegisterServerEvent("lvc:plugins_storePluginVersion") 
+AddEventHandler("lvc:plugins_storePluginVersion", function(name, version)
+	plugin_count = plugin_count + 1
+	plugins_cv[name] = version
+end)
+
+
+--[[CreateThread( function()
+-- Get LVC repo version from github
+	PerformHttpRequest("https://raw.githubusercontent.com/TrevorBarns/luxart-vehicle-control/master/version", function(err, responseText, headers)
+		if responseText ~= nil and responseText ~= "" then
+			repo_version = responseText
+		end
+	end)
+	
+	Wait(1000)
+	-- Get currently installed & enabled plugin versions (plugins -> 'lvc:plugins_storePluginVersion')
+	TriggerEvent('lvc:plugins_getVersions')
+	
+	-- Get repo version for installed & enabled plugins
+	for name, _ in pairs(plugins_cv) do
+		PerformHttpRequest("https://raw.githubusercontent.com/TrevorBarns/luxart-vehicle-control/master/PLUGINS/"..name.."/version", function(err, responseText, headers)
+			if responseText ~= nil and responseText ~= "" then
+				plugins_rv[name] = responseText
+			else
+				plugins_rv[name] = "unkn"
+			end
+		end)
+	end
+	Wait(1000)
+	print("\n\t^7 ________________________________________________________")   
+	print("\t|\t^8      __                       ^9___               ^7|")   
+	print("\t|\t^8     / /      ^7 /\\   /\\        ^9/ __\\              ^7|")   
+	print("\t|\t^8    / /        ^7\\ \\ / /       ^9/ /                 ^7|")   
+	print("\t|\t^8   / /___       ^7\\ V /       ^9/ /___               ^7|")   
+	print("\t|\t^8   \\____/uxart   ^7\\_/ ehicle ^9\\____/ontrol         ^7|")   
+	print("\t|\t                                                 |")   
+	print("\t|\t         INSTALLED VERSION: "..curr_version.."                |")   
+	print("\t|\t           LATEST VERSION:  "..repo_version.."                |") 
+	--	UPDATE DETECTED
+	if curr_version < repo_version then
+		print("\t|\t             ^8UPDATE REQUIRED                     ^7|")
+		print("\t|^8                      DOWNLOAD AT:                      ^7|")
+		print("\t|^2 github.com/TrevorBarns/luxart-vehicle-control/releases ^7|")
+	--	EXPERMENTAL VERSION 
+	elseif curr_version  > repo_version then
+		print("\t|\t           ^3EXPERIMENTAL VERSION                  ^7|")
+		-- IS THE USER AWARE THEY DOWNLOADED EXPERMENTAL CHECK CONVARS
+		if experimental == 'false' then
+		print("\t|^3 THIS VERSION IS IN DEVELOPMENT AND IS NOT RECOMMENDED  ^7|")
+		print("\t|^3 FOR PRODUCTION USE. IF THIS WAS A MISTAKE DOWNLOAD THE ^7|")
+		print("\t|^3 LATEST STABLE RELEASE AT:                              ^7|")
+		print("\t|^2 github.com/TrevorBarns/luxart-vehicle-control/releases ^7|")
+		print("\t|^3 TO MUTE THIS: SET CONVAR 'experimental' to 'true'      ^7|")
+		end
+	end
+	
+	--	IF PLUGINS ARE INSTALLED 
+	if plugin_count > 0 then
+		print("\t^7|________________________________________________________|") 			
+		print("\t^7|INSTALLED PLUGINS               | INSTALLED |  LATEST   |") 			
+		for name, version in pairs(plugins_cv) do
+			local plugin_string
+			if plugins_cv[name] < plugins_rv[name] then
+				plugin_string = string.format("\t|^8  %-30s^7|^8   %s   ^7|^8   %s   ^7|^8 UPDATE REQUIRED    ^7", name, plugins_cv[name], plugins_rv[name])					
+			elseif plugins_cv[name] > plugins_rv[name] then
+				plugin_string = string.format("\t|^3  %-30s^7|^3   %s   ^7|^3   %s   ^7|^3 EXPERIMENTAL VERSION ^7", name, plugins_cv[name], plugins_rv[name])					
+			else
+				plugin_string = string.format("\t|  %-30s|   %s   |   %s   |", name, plugins_cv[name], plugins_rv[name])
+			end
+			print(plugin_string)
+		end
+		print("\t^7|________________________________________________________|") 
+		print("\t^7|      Updates, Support, Feedback: ^5discord.link/LVC      ^7|")   
+		print("\t^7|________________________________________________________|\n\n") 
+	else
+		print("\t^7|________________________________________________________|") 
+		print("\t^7|      Updates, Support, Feedback: ^5discord.link/LVC      ^7|")   
+		print("\t^7|________________________________________________________|\n\n")   
+	end
+end)]]
